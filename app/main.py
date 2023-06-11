1ee389bf650672416cfca10769652910b296f422
Maddala Sravani <sravanimaddala1308@gmail.com>
2023-06-11 09:26:03 +0000

    Connecting DB

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+# from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 
 from app.schema import Book as SchemaBook
@@ -14,9 +18,12 @@ from app.models import Author as ModelAuthor
 import os
 from dotenv import load_dotenv
 
+
+
 load_dotenv('.env')
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 # to avoid csrftokenError
 app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
@@ -24,6 +31,12 @@ app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 @app.get("/")
 async def root():
    return {"message": "hello world"}
+
+@app.get("/index/", response_class=HTMLResponse)
+async def get_html():
+    with open("index.html") as file:
+        html_content = file.read()
+    return html_content
 
 
 @app.post('/book/', response_model=SchemaBook)
@@ -52,6 +65,27 @@ async def author():
    return author
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from fastapi import Depends
+engine = create_engine('postgresql://postgres:password123@postgres/fastapi')
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get('/templates/index2/', response_class=HTMLResponse)
+async def get_html(request: Request,db=Depends(get_db)):
+   books =  db.query(Book).all()
+   data = [book.__dict__ for book in books]  # Replace with your model data
+   return templates.TemplateResponse("index.html", {"request": request, "data": data})
+
+    
 # To run locally
 if __name__ == '__main__':
    uvicorn.run(app, host='0.0.0.0', port=8000)
